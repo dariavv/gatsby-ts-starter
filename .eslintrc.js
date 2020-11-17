636bbeb09af4b7fcbceebb655441d79536c5e1b3
dariavv <daria.sidorovich333@gmail.com>
2020-11-17 20:07:53 +0300

    feat: add aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
-const path = require("path")
-
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
@@ -18,27 +16,28 @@ module.exports = {
     node: true,
     es6: true,
   },
-  plugins: ["@typescript-eslint", "react", "prettier"],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
       arrowFunctions: true,
     },
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
     'import/prefer-default-export': 'off',
     'no-empty-function': 'off',
     'global-require': 'off',
-    "prettier/prettier": "error",
-    "react/prop-types": "off", 
-    "react/no-unescaped-entities": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    '@typescript-eslint/no-empty-function': "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
